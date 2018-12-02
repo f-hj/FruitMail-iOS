4bde37221ebf27770932601791c93c2cdd0ca210
Florian Hermouet-Joscht <hello@florianhj.fr>
2018-12-02 14:57:02 +0100

    Initial Commit

---
+++ FruitMail/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FruitMail
+//
+//  Created by Florian Hermouet-Joscht on 12/2/18.
+//  Copyright © 2018 Florian Hermouet-Joscht. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
